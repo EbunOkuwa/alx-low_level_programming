da5e9400f5e688281a1822b336e166a8182c8684
EbunOkuwa <ebunokuwa@gmail.com>
2022-10-27 16:45:31 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strncat - joins two strings with n bytes
- * @dest: copy to
- * @src: copy from
- * @n: number of char to be copied
+ * _strncat - concatenates two strings with n bytes
+ * @dest: destination
+ * @src: source
+ * @n: amount of bytes used from src
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n);
@@ -17,6 +17,7 @@ char *_strncat(char *dest, char *src, int n);
 	{
 		i++;
 	}
+
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
@@ -25,5 +26,6 @@ char *_strncat(char *dest, char *src, int n);
 		j++;
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
