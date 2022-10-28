52fa6e64c311c45f098e510a650e880f207b710e
EbunOkuwa <ebunokuwa@gmail.com>
2022-10-27 18:04:12 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,13 +6,14 @@
  * @n: number of elements of the array
  * Return: rev
  */
+
 void reverse_array(int *a, int n)
 {
 	int i, j, temp;
 
 	for (i = 0; i < n - 1; i++)
 	{
-		for (j = 1 + 1; j > 0; j--)
+		for (j = i + 1; j > 0; j--)
 		{
 			temp = *(a + j);
 			*(a + j) = *(a + (j - 1));
