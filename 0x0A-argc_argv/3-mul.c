32a71717064a8fccaa6ae58aaf0d9fd1aa65001f
EbunOkuwa <ebunokuwa@gmail.com>
2022-11-04 10:04:06 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		for (i = 1; i < argc, i++)
+		for (i = 1; i < argc; i++)
 			result = result * strtol(argv[i], NULL, 10);
 		printf("%d\n", result);
 	}
